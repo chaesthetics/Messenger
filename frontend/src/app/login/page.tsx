472aa0f72587e376a93328e7616f09c162fff833
Auriel James <aurieljames11@gmail.com>
2024-02-04 22:49:06 +0800

    issue/#1 LoginPage WOP 50%

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "@/components/navbar";
+
+
+const UsersPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                This is a Login Page
+            </div>
+        </div>
+    )
+}
+
+export default UsersPage;
