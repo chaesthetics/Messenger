4377c1fe1807a1116f67c6979c03e5e8d0449403
Auriel James <aurieljames11@gmail.com>
2024-07-10 00:37:17 +0800

    Setting Up Done

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 return(
     <div className="mt-16 flex justify-around px-24 space-x-4 text-sm py-14">
-        <p className="text-gray-800 hover:underline hover:cursor-pointer">© Meta 2024.</p> 
+        <p className="text-gray-800 hover:underline hover:cursor-pointer">© Auriel James 2024.</p> 
         <p className="text-gray-800 hover:underline hover:cursor-pointer">The Apple and Google Play logos are trademarks of their respective owners.</p>
         <p className="text-gray-800 hover:underline hover:cursor-pointer">Privacy Policy</p>
         <p className="text-gray-800 hover:underline hover:cursor-pointer">Cookie Policy</p>
