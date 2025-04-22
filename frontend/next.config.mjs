5e8e2947b4c69a4b7a76e8f2941c773df6c4d335
Auriel James Fernandez <aurieljames11@gmail.com>
2025-04-22 20:12:57 +0800

    Updated nextConfig to accept image from other domains

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        'scontent.fmnl17-4.fna.fbcdn.net',
+        'instagram.fmnl17-6.fna.fbcdn.net',
+        'www.instagram.com',
+    ],
+    },
+  };
+  
+  export default nextConfig;
