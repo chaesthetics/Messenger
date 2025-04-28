b433084cfbd8fb9cdc410cf13f674607f1c5fa22
Auriel James Fernandez <aurieljames11@gmail.com>
2025-04-28 22:01:02 +0800

    Added cors.php

---
+++ backend/config/cors.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+'supports_credentials' => false,
+
+'allowed_origins' => [
+    'http://35.232.205.217',  // Your frontend domain
+],
+
+'allowed_origins_patterns' => [],
+
+'allowed_headers' => ['*'],
+'allowed_methods' => ['*'],
+'exposed_headers' => [],
+'max_age' => 0,
+];
