86d52fc4217543e09df99a28ffc75eca8be3f046
Auriel James Fernandez <aurieljames11@gmail.com>
2025-04-29 20:01:11 +0800

    Added frontend domain to allowed_origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,6 +6,8 @@ return [
 
 'allowed_origins' => [
     'http://35.232.205.217',  // Your frontend domain
+    'https://messenger-chaesthetics-projects.vercel.app/login',
+    'https://messenger-nine-murex.vercel.app/login',
 ],
 
 'allowed_origins_patterns' => [],
