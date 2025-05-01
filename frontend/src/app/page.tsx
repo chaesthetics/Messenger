acafaa49c9e59e496ff914da816a535e9a1b4839
Auriel James Fernandez <aurieljames11@gmail.com>
2025-05-01 09:10:29 +0800

    Fixed production build error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
+'use client';
 import React from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const router = useRouter();
