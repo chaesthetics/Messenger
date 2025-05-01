6f4b5eae1160caa6c0f2a19012d22702c644757e
Auriel James Fernandez <aurieljames11@gmail.com>
2025-05-01 10:44:51 +0800

    feature/#56-navbar-and-footer-responsiveness

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,12 +1,12 @@
 const Footer = () => {
 return(
-    <div className="flex mt-16 flex justify-around px-24 space-x-4 text-sm py-14 overflow-hidden">
+    <div className="flex mt-8 md:mt-16 flex justify-around px-8 md:px-24 space-x-4 text-sm py-8 md:py-14 overflow-hidden">
         <p className="text-gray-800 hover:underline hover:cursor-pointer">© Auriel James 2024.</p> 
-        <p className="text-gray-800 hover:underline hover:cursor-pointer">The Apple and Google Play logos are trademarks of their respective owners.</p>
-        <p className="text-gray-800 hover:underline hover:cursor-pointer">Privacy Policy</p>
-        <p className="text-gray-800 hover:underline hover:cursor-pointer">Cookie Policy</p>
+        <p className="text-gray-800 hover:underline hover:cursor-pointer hidden md:block">The Apple and Google Play logos are trademarks of their respective owners.</p>
+        <p className="text-gray-800 hover:underline hover:cursor-pointer  hidden md:block">Privacy Policy</p>
+        <p className="text-gray-800 hover:underline hover:cursor-pointer hidden md:block">Cookie Policy</p>
         <p className="text-gray-800 hover:underline hover:cursor-pointer">Terms</p>
-        <p className="text-gray-800 hover:underline hover:cursor-pointer">English (US)</p>
+        <p className="text-gray-800 hover:underline hover:cursor-pointer hidden md:block">English (US)</p>
         <p className="text-gray-800 hover:underline hover:cursor-pointer">From Meta</p>
     </div>
 )
