5f02628620a7093e452d6a568b309ea3d08a0b03
Auriel James Fernandez <aurieljames11@gmail.com>
2025-05-03 09:10:59 +0800

    Image unoptimized

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: [
-        'scontent.fmnl17-4.fna.fbcdn.net',
-        'instagram.fmnl17-6.fna.fbcdn.net',
-        'www.instagram.com',
-        'instagram.fmnl17-8.fna.fbcdn.net',
-        'scontent.fmnl17-8.fna.fbcdn.net',
-        'scontent.fcrk4-1.fna.fbcdn.net',
-    ],
+      unoptimized: true,
     },
   };
   
-  export default nextConfig;
+export default nextConfig;
